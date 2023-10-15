0ccd0abd1dd559ec5710b067f0538937cfe0f0c3
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-10-14 23:53:13 -0300

    config
    config from a config archieve that define the port from the project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('deu certo');
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+//JSON middleware
+app.use(express.json())
+
+// app port
+const port = config.get<number>('port')
+app.listen(port, async () => {
+    console.log(`aplicação está funcionando na porta ${port}`);
+});

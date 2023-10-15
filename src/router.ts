69bff92938e0c832b5885770e69be9e3b9196b51
Gabriel-Vict0r <victorgabriel1730@gmail.com>
2023-10-15 19:14:29 -0300

    feat
    finish from all the routes from API

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies, removeMovie, updateMovie } from "./controllers/movieControllers";
 
 //validations
 import { validate } from './middleware/handleValidation'
@@ -10,3 +10,6 @@ const router = Router()
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send('API WORKING');
 }).post('/movie', movieCreateValidation(), validate, createMovie)
+    .get('/movie/:id', findMovieById).get('/movie', getAllMovies)
+    .delete('/movie/:id', removeMovie)
+    .patch("/movie/:id", movieCreateValidation(), validate, updateMovie);
